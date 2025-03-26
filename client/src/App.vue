436a9d27257cfdc4cb34380c3459a99dcdc56b80
patient <undefinedpatient@gmail.com>
2025-03-26 23:12:12 +0800

    Tutorial

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,39 @@
 <script setup>
+import { ref } from 'vue'
+
+// give each todo a unique id
+let id = 0
+
+const newTodo = ref('')
+const todos = ref([
+  { id: id++, text: 'Learn HTML' },
+  { id: id++, text: 'Learn JavaScript' },
+  { id: id++, text: 'Learn Vue' }
+])
+
+function addTodo() {
+  todos.value.push({ id: id++, text: newTodo.value })
+  newTodo.value = ''
+}
+
+function removeTodo(todo) {
+  todos.value = todos.value.filter((t) => t !== todo)
+}
+
+function onInput(e){
+  newTodo.value = e.target.value;
+}
 </script>
 
 <template>
-
+  <form @submit.prevent="addTodo">
+    <input :value="newTodo" @input="onInput" required placeholder="new todo">
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
 </template>
-
-<style scoped>
-</style>
