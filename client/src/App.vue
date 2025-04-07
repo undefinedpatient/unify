9267d6e0cab7d68795f6e32e71a125c0941c0f53
patient <undefinedpatient@gmail.com>
2025-04-07 12:55:28 +0800

    Update App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,20 +4,60 @@ let menuClass= ref("menuBar");
 </script>
 
 <template>
-  <div>Unify</div>
-  <div v-bind:class="menuClass">
-    <a href="">Home</a>
-    <a href="">About Us</a>
-    <a href="">Contact</a>
-    <a href="">Support Us</a>
+  <div class="home">
+    <h1>Unify</h1>
+  </div>
+  <div :class="menuClass">
+    <a href="#">Home</a>
+    <a href="#">About Us</a>
+    <a href="#">Contact</a>
+    <a href="#">Support Us</a>
   </div>
 </template>
 
 <style lang="css">
-  div{
+  *{
+    box-sizing: border-box; 
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    scroll-behavior: smooth;
+  }
+  div.home{
+    position:relative; 
+    width: 100vw;
+    height: 100vh;
     font-size: 2em;
+
+    background-image: url("./assets/nightsky.png");
+    background-size: cover;
+    &>h1{
+      position:absolute;
+      color:white;
+      font-size: 4em;
+      left: 30px;
+      bottom: 10px;
+    }
   }
   div.menuBar{
+    display: flex;
+      flex-direction: row;
+      justify-content: left;
+    background-color: rgb(110, 110, 110);
     font-size:1em;
+  }
+  a{
+    display: block;
+    position: relative;
+    padding: 0px 10px 0px 10px;
+    background-color: rgba(255, 255, 255, 0);
+    
+    font-size: 2em;
+    text-decoration: none;
+    color:rgb(255, 255, 255);
+    &:hover{
+      background-color: rgba(255, 255, 255, 0.1);
+    }
   }
 </style>
