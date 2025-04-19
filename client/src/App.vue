cc0cb501faa4590f4f424a425db6988a0818bf0e
patient <undefinedpatient@gmail.com>
2025-04-19 15:23:08 +0800

    Add Basic Structure + Footer

--- client/src/App.vue
+++ client/src/App.vue
@@ -17,8 +17,38 @@ let menuClass= ref("menuBar");
   </div>
   <div class="aboutUs" id="aboutUs">
     <h2>About Us</h2>
-    <p>This is a p</p>
+    <p>
+      Welcome to Vertex, an indie animation studio crafting vibrant, 
+      heartfelt stories that captivate and inspire. With a passionate team of artists and storytellers, 
+      we blend innovative techniques with timeless narratives to create unique animated experiences. 
+      From whimsical shorts to bold feature films, our work celebrates creativity and connection. 
+      Join us on this colorful journey!
+    </p>
   </div>
+  <div class="news" id="news">
+    <h2>News</h2>
+    <p>
+    </p>
+  </div>
+  <div class="contacts" id="contacts">
+    <h2>Contacts</h2>
+    <p>
+    </p>
+  </div>
+  <div class="supportUs" id="supportUs">
+    <h2>Support Us</h2>
+    <p>
+    </p>
+  </div>
+  <footer>
+    <p>&copy; 2025 Vertex. All rights reserved.</p>
+    <div class="contact-links">|
+      <a href="#">contact@gmail.com</a> |
+      <a href="#">+852 1234 5678</a> |
+      <a href="#">Discord</a> |
+      <a href="#">X</a> |
+    </div>
+  </footer>
 </template>
 
 <style lang="css">
@@ -66,9 +96,9 @@ let menuClass= ref("menuBar");
     position: relative;
     display: block;
     width: 100vw;
-    height: 100vw;
-    padding: 16px;
-    font-size: 1em;
+    height: max-content;
+    padding: 64px;
+    font-size: 1rem;
     background-color: var(--layer00);
     background-image: 
     linear-gradient(0deg,rgba(24, 24, 24, 0.8),50%, rgba(20, 20, 20, 0.5)),
@@ -77,7 +107,56 @@ let menuClass= ref("menuBar");
     background-size: cover;
     & p{
       font-size: 1em;
+      max-width: 800px;
     }
     
   }
+  div.news{
+    position: relative;
+    display: block;
+    width: 100vw;
+    height: max-content;
+    padding: 64px;
+    font-size: 1rem;
+    background-color: var(--layer01);
+  }
+  div.contacts{
+    position: relative;
+    display: block;
+    width: 100vw;
+    height: max-content;
+    padding: 64px;
+    font-size: 1rem;
+    background-color: var(--layer01);
+  }
+  div.supportUs{
+    position: relative;
+    display: block;
+    width: 100vw;
+    height: max-content;
+    padding: 64px;
+    font-size: 1rem;
+    background-color: var(--layer01);
+  }
+  footer{
+    position:relative;
+    display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+    background-color: var(--layer00);
+    color: #ecf0f1;
+    font-style: italic;
+    padding: 20px;
+    width: 100%;
+    & p{
+      color: #ecf0f1;
+    }
+    & .contact-links a{
+      text-decoration: none;
+    }
+  }
+  @media (min-width: 600px) {
+    
+  }
+
 </style>
